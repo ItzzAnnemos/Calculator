e8868bd95a37600eb999e9ab95fa7c64c94f210f
Nikola <serafimovnikola582@gmail.com>
2026-03-04 21:48:16 +0100

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Nikola Serafimov on 4.3.26.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
